ada553484b18966b27535c3c3cfbcdaa685a7d45
Vishal1720 <89019986+Vishal1720@users.noreply.github.com>
2022-12-15 20:13:51 +0530

    Update project.js
    added functions to turn buttons to green and red as per conditions

--- project.js
+++ project.js
@@ -1,18 +1,43 @@
 let players = document.getElementById("player");
-
 let buns=document.getElementsByClassName("clikBtn");
+let attempt=document.getElementById("attempts");
 var inc,winCondition;
-prompter();
+var gameend=false;
+var i=0;
+prompter();//calling function to ask for user name
 
-
-
-
-function check(b)
+function generateTreasureIndex()//Use to select a number for index where the treasure is there
 {
-   
-    // if(winCondition=== b)
-for(var i=0;i<buns.length;i++)
-buns[i].value="O";
+     winCondition=Math.floor(Math.random()*16);
+}
+generateTreasureIndex();
+
+function check(id)
+  {
+    if(gameend==false) 
+        {
+    console.log("Value is "+winCondition);
+    console.log(buns.length);
+    if(buns[id].style.backgroundColor=="red"||buns[id].style.backgroundColor=="green")
+    {
+        //This to prevent unecessary increment to attempts
+    }
+    else
+    {
+        if(id===winCondition)
+         {
+          buns[id].style.backgroundColor="green";
+          gameend = true;
+          }
+       else
+        {
+        buns[id].style.backgroundColor="red";
+           }
+    i++;
+    attempt.textContent="Attempts:"+i;//attempts updated
+    }
+
+}
 }
 
 
@@ -20,6 +45,7 @@ function prompter()
 {
      var a=prompt("Enter your name");
      
-     players.textContent+=a;
-     console.log(a);
+     players.textContent+=a;//name added to para 
+     
 }
+
